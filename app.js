a5eb95eed94220a16b0cc233dd9eb75e0a791fa7
justin <jz7233@rit.edu>
2023-12-13 16:25:48 -0500

    initial mongodb setup 2

--- app.js
+++ app.js
@@ -5,9 +5,28 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 
+
+//include config file for database
+const config = require('./config/database');
+
+
+//connect to MongoDB using config file variable
+mongoose.connect(config.database);
+
+//check for connection/error and log
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database '+config.database)
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Databse error '+err)
+});
+
+
 //initialize app variable
 const app = express();
 
+//bring in users file for routes
 const users = require('./routes/users');
 
 //create port number
@@ -16,7 +35,7 @@ const port = 3000;
 //cors middleware
 app.use(cors());
 
-//set static folder
+//set static folder to public
 app.use(express.static(path.join(__dirname, 'public')));
 
 //body parser middleware
